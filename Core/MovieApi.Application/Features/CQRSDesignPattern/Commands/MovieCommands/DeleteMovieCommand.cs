ff922747ecf70cd93f095c4ec08e3b9f697bf732
bbiircan <bircanbircan1963@gmail.com>
2025-03-04 14:14:20 +0300

    Added scoped services for category and movie handlers, and implemented the movie controller

--- Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/MovieCommands/DeleteMovieCommand.cs
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/MovieCommands/DeleteMovieCommand.cs
@@ -2,6 +2,11 @@
 {
     public class DeleteMovieCommand
     {
+        public DeleteMovieCommand(int movieId)
+        {
+            MovieId = movieId;
+        }
+
         public int MovieId { get; set; }
     }
 }
