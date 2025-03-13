d046d2427bd1b92fc2ebe1fa534467f0016fdeca
bbiircan <bircanbircan1963@gmail.com>
2025-03-13 18:36:21 +0300

    Implemented MediatR queries and commands for managing and retrieving cast data, including Create, Update, Delete, GetById, and GetAll operations.

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/CastCommands/DeleteCastCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands
+{
+    public class DeleteCastCommand : IRequest
+    {
+        public int CastId { get; set; }
+
+        public DeleteCastCommand(int castId)
+        {
+            CastId = castId;
+        }
+    }
+}
