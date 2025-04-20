36a41d877c12f80f10d9dfec387fc51728750fda
bbiircan <bircanbircan1963@gmail.com>
2025-04-20 16:44:14 +0300

    Added CastsController and implemented MediatR commands and queries for Cast and Tag.

---
+++ Presentation/MovieApi.WebAPI/Controllers/CastsController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+
+namespace MovieApi.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IActionResult CastList()
+        {
+            var value = _mediator.Send(new GetCastQuery());
+            return Ok(value); 
+        }
+
+        [HttpPost]
+        public IActionResult CreateCast(CreateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("The cast has been successfully added.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCast(int id) 
+        {
+            _mediator.Send(new DeleteCastCommand(id));
+            return Ok("The cast has been successfully deleted.");
+        }
+
+        [HttpGet("GetCastById")]
+        public IActionResult GetCastById (int id)
+        {
+            var value = _mediator.Send(new GetCastByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCast(UpdateCastCommand command)
+        {
+            _mediator.Send(command);
+            return Ok("The cast has been successfully updated.");
+        }
+
+    }
+}
