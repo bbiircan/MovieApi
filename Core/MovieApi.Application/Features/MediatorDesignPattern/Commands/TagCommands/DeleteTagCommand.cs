06c418a6d326dcc7343118f7b86df0863d89667a
bbiircan <bircanbircan1963@gmail.com>
2025-05-06 14:16:39 +0300

    Implemented MediatR design pattern for tag operations.

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/TagCommands/DeleteTagCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.TagCommands
+{
+    public class DeleteTagCommand : IRequest
+    {
+        public int TagId { get; set; }
+
+        public DeleteTagCommand(int tagId)
+        {
+            TagId = tagId;
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/TagHandlers/DeleteTagCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.TagCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers
+{
+    public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand>
+    {
+        private readonly MovieContext _context;
+
+        public DeleteTagCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tags.FindAsync(request.TagId);
+            _context.Tags.Remove(values);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
