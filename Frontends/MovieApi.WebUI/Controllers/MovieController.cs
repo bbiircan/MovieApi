3345b8983ee9be849f52de52f6c8e68146533a7e
bbiircan <bircanbircan1963@gmail.com>
2025-05-19 19:32:39 +0300

    Movies were added to the homepage.

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
 
 namespace MovieApi.WebUI.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
         {
             ViewBag.v1 = "Movie List";
             ViewBag.v2 = "Home";
             ViewBag.v3 = "All Movies";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7140/api/Movies");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
