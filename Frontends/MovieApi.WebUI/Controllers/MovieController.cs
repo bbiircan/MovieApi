d8392d0be8627653f5fe1ef8262f6d1a8b422dbb
bbiircan <bircanbircan1963@gmail.com>
2025-05-19 17:16:53 +0300

    Layout Operations Completed.

---
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            ViewBag.v1 = "Movie List";
+            ViewBag.v2 = "Home";
+            ViewBag.v3 = "All Movies";
+            return View();
+        }
+    }
+}
